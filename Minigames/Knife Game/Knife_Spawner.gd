27e2f0e1bcb647074de459888df4cc52080685a4
Walter <walterdirvin@gmail.com>
2021-09-18 00:07:25 -0400

    created knife spawner

--- Minigames/Knife Game/Knife_Spawner.gd
+++ Minigames/Knife Game/Knife_Spawner.gd
@@ -1,12 +1,26 @@
 extends Node2D
-
+var knife = load("Minigames/Knife Game/Knife.tscn")
+var set_spawn = false
+var max_delay = 2.0
+var delay = 0.0
 func _ready():
 	pass # Replace with function body.
 
 func _input(event):
-	if event == InputEventMouseButton:
-		pass
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				if delay <= 0.0:
+					delay = 0.0
+					set_spawn = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	if set_spawn:
+		delay = max_delay
+		set_spawn = false
+		var new_knife = knife.instance()
+		new_knife.position = position
+		self.add_child(new_knife)
+	else:
+		delay -= delta
