28cac60459a5a7f2ae523ed98d1a20ebcee4f96f
Walter <walterdirvin@gmail.com>
2021-09-17 23:23:43 -0400

    .import push

--- Minigames/Knife Game/Knife_Spawner.gd
+++ Minigames/Knife Game/Knife_Spawner.gd
@@ -3,7 +3,9 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-
+func _input(event):
+	if event == InputEventMouseButton:
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
