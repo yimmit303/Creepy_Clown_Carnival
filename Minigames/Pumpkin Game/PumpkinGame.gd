c02899b5ec3ea0f57e37239b55f2a675d61bad9c
yimmit303 <timsteiner303@gmail.com>
2021-09-18 18:55:45 -0400

    Pumpkin Game mockup

---
+++ Minigames/Pumpkin Game/PumpkinGame.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var active = false
+var num_popped = 0
+
+func make_active():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	$BalloonSpawner.spawn_balloons()
+	$DartThrower.can_throw = true
+	$Timer.counting = true
+	$Music.play()
+
+func make_inactive():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$DartThrower.can_throw = false
+	$Timer.counting = false
+	$Music.stop()
+
+func _on_game_won():
+	make_inactive()
+	$BalloonSpawner.pop_all()
+	$WinScreen.play()
+	yield($WinScreen, "done_playing")
+
+func _on_game_lost():
+	if num_popped < 3: # This stops the win and loss from happening at the same time
+		make_inactive()
+		$BalloonSpawner.pop_all()
+		$LoseScreen.play()
+		yield($LoseScreen, "done_playing")
+
+func _on_balloon_popped():
+	num_popped += 1
+	if num_popped == 3:
+		_on_game_won()
